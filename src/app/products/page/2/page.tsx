586a7dcb56de4c01272fd90af8f50b5ac6bf72f7
abdourrazak <abdourrazakahmadou250@gmail.com>
2025-11-20 09:07:01 +0100

    ✨ Create Shop Page 2 with slide1-slide8 images, minimalist card design & working pagination

---
+++ src/app/products/page/2/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components/layout/Header'
+import { Footer } from '@/components/layout/Footer'
+import { ShopContentPage2 } from '@/components/shop/ShopContentPage2'
+
+export default function ShopPage2() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <ShopContentPage2 />
+      <Footer />
+    </div>
+  )
+}
